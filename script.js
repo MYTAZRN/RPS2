ccc9b633c88f25fd400303f8109d1b8dfef06b2a
MYTAZRN <colormarauder@gmail.com>
2025-03-05 21:09:25 -0800

    final draft of rps

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-function getComputerChoice() {
-    const compChoice = Math.floor(Math.random()* 3);
-    switch (compChoice) {
-        case 0: 
-        console.log("rock")
-        break;
-        case 1:
-        console.log("paper")
-        break;
-        case 2:
-        console.log("scissors")
-        break;
-    }
-}
-    
+/**
+ * getComputerChoice returns 3 random choices = rock,paper,scissors.
+ */
 
-getComputerChoice()
+function getComputerChoice() {
+    let choices = Math.floor(Math.random() * 3);
+    return choices;
+}
+
+//getComputerChoice()
 
 function getHumanChoice() {
-    let forPlayer = prompt("Lets play Rock,Paper,Scissors");
-    let userInput = forPlayer.toLowerCase();
-    if (userInput === "rock"){
-        console.log("rock");
-    }
-    else if (userInput === "paper"){
-        console.log("paper");
-    }
-    else if (userInput === "scissors"){
-        console.log("scissors");
-    }
-    
+    let display = prompt ("Lets play Rock, Paper, Scissors!");
+    return display;
 }
-getHumanChoice()
+
+//getHumanChoice()
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice,computerChoice) {
+   humanChoice = getHumanChoice();
+   computerChoice = getComputerChoice();
+   if (humanChoice === computerChoice){
+    humanScore++ && computerScore++;
+   }
+   else if (
+    humanChoice === "rock" && computerChoice === "scissors" ||
+    humanChoice === "scissors" && computerChoice === "paper" ||
+    humanChoice === "paper" && computerChoice === "rock" 
+   ){
+    humanScore++;
+   } else {
+    computerScore++;
+   }
 
 }
+
+
+playRound();
+
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        playRound();
+        
+    }
+}
+playGame()
